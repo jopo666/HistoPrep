c3ca99f0fd98641b4a588d889074237a241b7626
jopo666 <jopo@birdlover.com>
2023-02-23 13:00:37 +0200

    remove catch errors

--- histoprep/backend/_exceptions.py
+++
@@ -1,6 +0,0 @@
-class SlideReadingError(Exception):
-    """Exception class for failures during slide reading."""
-
-
-class TileReadingError(Exception):
-    """Exception class for failures during tile reading."""
