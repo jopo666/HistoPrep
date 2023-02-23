2e00714dcb7c2b4f74643db09ab7c0d2d048df55
jopo666 <jopo@birdlover.com>
2023-02-23 13:01:38 +0200

    update tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,17 @@
 import os
+from pathlib import Path
 
 from PIL import Image
 
 from histoprep import functional as F
 
-DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
-TILE_PATH = os.path.join(DATA_DIR, "tile.jpeg")
-SLIDE_PATH_JPEG = os.path.join(DATA_DIR, "slide.jpg")
+DATA_DIR = Path(__file__).parent / "data"
+TILE_PATH = DATA_DIR / "tile.jpeg"
+SLIDE_PATH_JPEG = DATA_DIR / "slide.jpeg"
+SLIDE_PATH_MRXS = DATA_DIR / "tma_slide.mrxs"
+SLIDE_PATH_SVS = DATA_DIR / "slide.svs"
+SLIDE_PATH_CZI = DATA_DIR / "slide.czi"
+SLIDE_PATH_TMA = SLIDE_PATH_MRXS
 
 TILE_IMAGE = Image.open(TILE_PATH)
 TILE_THRESHOLD, TILE_MASK = F.detect_tissue(TILE_IMAGE)
