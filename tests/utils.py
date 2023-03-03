6e3759d0d826e847c8d3504038c5a42e319e345d
jopo666 <jopo@birdlover.com>
2023-03-03 08:55:24 +0200

    add test utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,19 +1,26 @@
 import os
+import shutil
 from pathlib import Path
 
 from PIL import Image
 
 from histoprep import functional as F
 
-DATA_DIR = Path(__file__).parent / "data"
-TILE_PATH = DATA_DIR / "tile.jpeg"
-SLIDE_PATH_JPEG = DATA_DIR / "slide.jpeg"
-SLIDE_PATH_MRXS = DATA_DIR / "tma_slide.mrxs"
-SLIDE_PATH_SVS = DATA_DIR / "slide.svs"
-SLIDE_PATH_CZI = DATA_DIR / "slide.czi"
+DATA_DIRECTORY = Path(__file__).parent / "data"
+TMP_DIRECTORY = DATA_DIRECTORY.parent / "tmp"
+TILE_PATH = DATA_DIRECTORY / "tile.jpeg"
+SLIDE_PATH_JPEG = DATA_DIRECTORY / "slide.jpeg"
+SLIDE_PATH_MRXS = DATA_DIRECTORY / "slide_tma.mrxs"
+SLIDE_PATH_SVS = DATA_DIRECTORY / "slide.svs"
+SLIDE_PATH_CZI = DATA_DIRECTORY / "slide.czi"
 SLIDE_PATH_TMA = SLIDE_PATH_MRXS
 
 TILE_IMAGE = Image.open(TILE_PATH)
-TILE_THRESHOLD, TILE_MASK = F.detect_tissue(TILE_IMAGE)
+TILE_THRESHOLD, TILE_MASK = F.get_tissue_mask(TILE_IMAGE)
 SLIDE_IMAGE = Image.open(SLIDE_PATH_JPEG)
-SLIDE_THRESHOLD, SLIDE_MASK = F.detect_tissue(SLIDE_IMAGE)
+SLIDE_THRESHOLD, SLIDE_MASK = F.get_tissue_mask(SLIDE_IMAGE)
+
+
+def clean_temporary_directory():
+    if TMP_DIRECTORY.exists():
+        shutil.rmtree(TMP_DIRECTORY)
