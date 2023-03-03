8b679b22255019ba02669dc09ffa1e82fada26bd
jopo666 <jopo@birdlover.com>
2023-03-03 08:55:37 +0200

    add message

--- histoprep/_errors.py
+++ histoprep/_errors.py
@@ -2,4 +2,8 @@ __all__ = ["LevelNotFoundError"]
 
 
 class LevelNotFoundError(Exception):
-    pass
+    def __init__(self, *args: object) -> None:
+        self.args = args
+
+    def __str__(self, *args: object) -> None:
+        return "Level {} not found, choose from {}.".format(*self.args)
