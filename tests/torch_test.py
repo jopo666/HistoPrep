6c38a535ac57908d2e3697b0f6ff79b030476044
jopo666 <jopo@birdlover.com>
2023-03-16 14:07:42 +0200

    update import and tissue mask

--- tests/torch_test.py
+++ tests/torch_test.py
@@ -4,12 +4,12 @@ from torch.utils.data import DataLoader, Dataset
 from histoprep import SlideReader
 from histoprep.utils import SlideReaderDataset
 
-from .utils import SLIDE_PATH_CZI
+from ._utils import SLIDE_PATH_CZI
 
 
 def test_torch_dataset() -> None:
     reader = SlideReader(SLIDE_PATH_CZI)
-    tissue_mask = reader.get_tissue_mask()
+    __, tissue_mask = reader.get_tissue_mask()
     coords = reader.get_tile_coordinates(tissue_mask, 512, max_background=0.01)
     # CZI fails if multiple workers read data from same instance, which should not
     # happen here as there is some voodoo shit going on with `Dataset` & `DataLoader`...
