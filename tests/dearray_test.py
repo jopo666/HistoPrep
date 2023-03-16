a5f75737e50be6d1d62a0e6148e7b2b17199f365
jopo666 <jopo@birdlover.com>
2023-03-16 13:16:00 +0200

    update dearray tests

--- tests/dearray_test.py
+++ tests/dearray_test.py
@@ -4,45 +4,30 @@ import pytest
 
 import histoprep as hp
 import histoprep.functional as F
-from tests.utils import SLIDE_PATH_TMA
+from tests._utils import SLIDE_PATH_TMA
+
+IMAGE = hp.SlideReader(SLIDE_PATH_TMA).read_level(-1)
 
 
-def test_dearray() -> None:
-    reader = hp.SlideReader(SLIDE_PATH_TMA)
-    # Good sigma value.
-    tissue_mask = reader.get_tissue_mask(sigma=2).mask
+def test_dearray_good_mask() -> None:
+    __, tissue_mask = F.get_tissue_mask(IMAGE, sigma=2)
     tissue_mask_clean = F.clean_tissue_mask(tissue_mask)
     with warnings.catch_warnings():
         warnings.simplefilter("error")
         spots = F.dearray_tma(tissue_mask_clean)
-    assert len(spots) == 38
-    # Bad sigma value.
-    tissue_mask = reader.get_tissue_mask(sigma=0).mask
-    tissue_mask_clean = F.clean_tissue_mask(tissue_mask)
+    assert len(spots) == 94
+
+
+def test_dearray_bad_mask() -> None:
+    __, tissue_mask = F.get_tissue_mask(IMAGE, sigma=0.0)
     with pytest.warns():
-        spots = F.dearray_tma(tissue_mask_clean)
-    # Bad sigma, good cleaning
-    tissue_mask_clean = F.clean_tissue_mask(tissue_mask, min_area_pixel=200)
-    with warnings.catch_warnings():
-        warnings.simplefilter("error")
-        spots = F.dearray_tma(tissue_mask_clean)
-    assert len(spots) == 38
-    # Empty mask
+        F.dearray_tma(tissue_mask)
+
+
+def test_dearray_empty_mask() -> None:
+    __, tissue_mask = F.get_tissue_mask(IMAGE)
     tissue_mask[...] = 0
     with warnings.catch_warnings():
         warnings.simplefilter("error")
         spots = F.dearray_tma(tissue_mask)
-        assert len(spots) == 0
-
-
-def test_clean_mask() -> None:
-    reader = hp.SlideReader(SLIDE_PATH_TMA)
-    tissue_mask = reader.get_tissue_mask(sigma=2).mask
-    empty_mask = tissue_mask.copy()
-    empty_mask[...] = 0
-    with warnings.catch_warnings():
-        warnings.simplefilter("error")
-        # empty mask.
-        F.clean_tissue_mask(empty_mask)
-        # bad values.
-        F.clean_tissue_mask(tissue_mask, max_area_pixel=0)
+    assert len(spots) == 0
