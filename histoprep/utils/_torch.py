9a8a91b06d328e3c79ab91f69a6419756b450e78
jopo666 <jopo@birdlover.com>
2023-03-18 09:53:22 +0200

    add tile image dataset

--- histoprep/utils/_torch.py
+++ histoprep/utils/_torch.py
@@ -1,23 +1,31 @@
 from __future__ import annotations
 
-__all__ = ["SlideReaderDataset"]
+__all__ = ["SlideReaderDataset", "TileImageDataset"]
 
+import ctypes
+import math
+import multiprocessing
 from collections.abc import Callable, Iterator
+from pathlib import Path
 from typing import Any
 
 import numpy as np
+from PIL import Image
 
 from histoprep._reader import SlideReader
 
 try:
-    from torch.utils.data import Dataset
+    from torch.utils.data import Dataset, IterableDataset
 
     HAS_PYTORCH = True
 except ImportError:
     HAS_PYTORCH = False
     Dataset = object
+    IterableDataset = object
 
 ERROR_PYTORCH = "Could not import torch, make sure it has been installed!"
+ERROR_LENGTH_MISMATCH = "Path length ({}) does not match label length ({})."
+ERROR_TILE_SHAPE = "Tile shape must be defined to create a cache array."
 
 
 class SlideReaderDataset(Dataset):
@@ -28,7 +36,7 @@ class SlideReaderDataset(Dataset):
         level: int = 0,
         transform: Callable[[np.ndarray], Any] | None = None,
     ) -> None:
-        """Torch dataset yielding tile images.
+        """Torch dataset yielding tile images from reader.
 
         Args:
             reader: `SlideReader` instance.
@@ -53,3 +61,69 @@ class SlideReaderDataset(Dataset):
         if self.transform is not None:
             tile = self.transform(tile)
         return tile, np.array(xywh)
+
+
+class TileImageDataset(Dataset):
+    def __init__(
+        self,
+        paths: list[str | Path],
+        *,
+        labels: list[str] | None = None,
+        transform: Callable[[np.ndarray], Any] | None = None,
+        use_cache: bool = False,
+        tile_shape: tuple[int, ...] | None = None,
+    ) -> None:
+        """Torch dataset yielding tile images from paths.
+
+        Args:
+            paths: Paths to tile images.
+            labels: Indexable list of labels for each path. Defaults to None.
+            transform: Transform function for tile images.. Defaults to None.
+            use_cache: Cache each image to shared array, requires that each tile has the
+                same shape. Defaults to False.
+            tile_shape: Tile shape for creating a shared cache array. Defaults to None.
+        """
+        super().__init__()
+        if not HAS_PYTORCH:
+            raise ImportError(ERROR_PYTORCH)
+        if labels is not None and len(paths) != len(labels):
+            raise ValueError(ERROR_LENGTH_MISMATCH.format(len(paths), len(labels)))
+        if use_cache and tile_shape is None:
+            raise ValueError(ERROR_TILE_SHAPE)
+        self.paths = paths
+        self.labels = labels
+        self.transform = transform
+        self._use_cache = use_cache
+        self._cached_indices = set()
+        self._cache_array = None
+        if self._use_cache:
+            self._cache_array = create_shared_array(
+                num_samples=len(self.paths), shape=tile_shape
+            )
+
+    def __len__(self) -> int:
+        return len(self.paths)
+
+    def __getitem__(self, index: int) -> tuple[np.ndarray, str] | tuple[Any, str]:
+        path = self.paths[index]
+        if self._use_cache:
+            if index not in self._cached_indices:
+                self._cache_array[index] = np.array(Image.open(path))
+                self._cached_indices.add(index)
+            image = self._cache_array[index]
+        else:
+            image = np.array(Image.open(path))
+        if self.transform is not None:
+            image = self.transform(image)
+        labels = () if self.labels is None else self.labels[index]
+        if not isinstance(labels, (tuple, list)):
+            labels = (labels,)
+        return image, path, *labels
+
+
+def create_shared_array(num_samples: int, shape: tuple[int, ...]) -> np.ndarray:
+    shared_array_base = multiprocessing.Array(
+        ctypes.c_uint8, num_samples * math.prod(shape)
+    )
+    shared_array = np.ctypeslib.as_array(shared_array_base.get_obj())
+    return shared_array.reshape(num_samples, *shape)
--- tests/torch_test.py
+++ tests/torch_test.py
@@ -1,13 +1,22 @@
+import numpy as np
 import torch
 from torch.utils.data import DataLoader, Dataset
 
 from histoprep import SlideReader
-from histoprep.utils import SlideReaderDataset
+from histoprep.utils import (
+    SlideReaderDataset,
+    TileImageDataset,
+)
 
-from ._utils import SLIDE_PATH_CZI
+from ._utils import (
+    SLIDE_PATH_CZI,
+    SLIDE_PATH_JPEG,
+    TMP_DIRECTORY,
+    clean_temporary_directory,
+)
 
 
-def test_torch_dataset() -> None:
+def test_reader_dataset_loader() -> None:
     reader = SlideReader(SLIDE_PATH_CZI)
     __, tissue_mask = reader.get_tissue_mask()
     coords = reader.get_tile_coordinates(tissue_mask, 512, max_background=0.01)
@@ -21,3 +30,43 @@ def test_torch_dataset() -> None:
     assert isinstance(batch_images, torch.Tensor)
     assert batch_coords.shape == (4, 4)
     assert isinstance(batch_coords, torch.Tensor)
+
+
+def test_tile_dataset_loader() -> None:
+    clean_temporary_directory()
+    reader = SlideReader(SLIDE_PATH_JPEG)
+    metadata = reader.save_regions(TMP_DIRECTORY, reader.get_tile_coordinates(None, 96))
+    dataset = TileImageDataset(
+        metadata["path"].to_numpy(),
+        labels=metadata[list("xywh")].to_numpy(),
+        transform=lambda x: x[..., 0],
+    )
+    batch_images, batch_paths, batch_coords = next(
+        iter(DataLoader(dataset, batch_size=32))
+    )
+    clean_temporary_directory()
+    assert batch_images.shape == (32, 96, 96)
+    assert len(batch_paths) == 32
+    assert batch_coords.shape == (32, 4)
+
+
+def test_tile_dataset_cache() -> None:
+    clean_temporary_directory()
+    reader = SlideReader(SLIDE_PATH_JPEG)
+    metadata = reader.save_regions(TMP_DIRECTORY, reader.get_tile_coordinates(None, 96))
+    dataset = TileImageDataset(
+        metadata["path"].to_numpy(),
+        labels=metadata[list("xywh")].to_numpy(),
+        transform=lambda x: x[..., 0],
+        use_cache=True,
+        tile_shape=(96, 96, 3),
+    )
+    batch_images, batch_paths, batch_coords = next(
+        iter(DataLoader(dataset, batch_size=32))
+    )
+    clean_temporary_directory()
+    assert batch_images.shape == (32, 96, 96)
+    assert len(batch_paths) == 32
+    assert batch_coords.shape == (32, 4)
+    assert dataset._cached_indices == set(range(32))
+    assert np.equal(dataset._cache_array[0][..., 0], batch_images[0].numpy()).all()
